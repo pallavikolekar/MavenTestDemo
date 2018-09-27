174c9332eb5bb2962862e279ca1a7d78f879bf56
pallavim <pallavik@glam.com>
2018-09-27 13:19:48 -0700

    This is my first commit in Maven Project

---
+++ src/test/java/com/sampledemo/tests/MyFirstDemotest.java
@@ -0,0 +1,43 @@
+package com.sampledemo.tests;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.*;
+
+public class MyFirstDemotest {
+	
+	public WebDriver driver;
+	
+	@BeforeClass
+	public void Initialise() {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Pallavi\\eclipse-workspace\\FaceBookDemo\\drivers\\chromedriver.exe");
+		driver = new ChromeDriver();
+		
+	}
+	
+	
+	@Test
+	public void openBrowserurl() {
+		
+		driver.get("http://www.google.com");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+		driver.findElement(By.linkText("Gmail1")).click();
+		System.out.println("Gmail is clicked");
+		
+		
+	}
+	
+	
+	
+	@AfterClass
+	public void CloseBrowser() {
+		driver.quit();
+	}
+	
+
+}
